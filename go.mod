3b2ffad95f1f385b8af9d5c6f800c48e9cf9ea72
evolonation <1838940019@qq.com>
2023-02-14 23:14:11 +0800

    first commit, basic test

---
+++ go.mod
@@ -0,0 +1,12 @@
+module patient-edge
+
+go 1.19
+
+require github.com/eclipse/paho.mqtt.golang v1.4.2
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/pkg/errors v0.9.1
+	golang.org/x/net v0.0.0-20200425230154-ff2c4b7c35a0 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+)
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/pkg/errors"
+)
+
+const (
+	IP             = "localhost"
+	ClientId       = "PatientChecker-01"
+	SubscribeTopic = "$patient/sensor/+/temperature"
+)
+
+// connect connect to the Mqtt server.
+func connect() (client mqtt.Client, err error) {
+	defer func() {
+		if err != nil {
+			err = errors.Wrap(err, "mqtt connect error")
+		}
+	}()
+	opts := mqtt.NewClientOptions().AddBroker(IP).SetClientID(ClientId).SetCleanSession(true)
+	client = mqtt.NewClient(opts)
+	if tc := client.Connect(); tc.Wait() && tc.Error() != nil {
+		return nil, tc.Error()
+	}
+	return client, nil
+}
+
+func onMessage(client mqtt.Client, message mqtt.Message) {
+	log.Printf("receive message %s", message.Payload())
+	message.Topic()
+}
+
+func main() {
+	var err error
+	client, err := connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if tc := client.Subscribe(SubscribeTopic, 0, onMessage); tc.Wait() && tc.Error() != nil {
+		log.Fatal(tc.Error())
+	}
+}
