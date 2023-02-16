b493920e01c4e90b38c06ed9b6b4873924aec8a4
evolonation <1838940019@qq.com>
2023-02-16 12:16:30 +0800

    完成自动化构建镜像、部署

---
+++ build.sh
@@ -0,0 +1,27 @@
+# /bin/bash
+VERSION=v1.0.5
+
+# 卸载之前的部署
+. ./offload.sh
+
+# 创建target文件夹
+mkdir target
+cp deployment.yaml target
+
+# 修改deployment.yaml
+sed -i "s/CUP_VERSION/${VERSION}/g" target/deployment.yaml
+
+# 构建docker镜像
+docker build . -t evolonation/patient-edge:${VERSION} --platform arm64 --push
+
+# 创建configmap
+kubectl create configmap edge-mysql-scripts --from-file script.sql 
+
+# 部署
+kubectl apply -f target/deployment.yaml
+
+# 清除残余
+rm -r target
+
+
+
--- image.sh
+++
@@ -1,4 +0,0 @@
-# /bin/bash
-VERSION=v1.0.4
-IMAGENAME=patient-edge:${VERSION}
-docker build . -t ${IMAGENAME}
