44a511820a874ca2daf17f946cb4aa8490c20974
evolonation <1838940019@qq.com>
2023-02-16 10:37:56 +0800

    1、mysql的pod引用configmap执行建表语句 2、添加mysql建表语句脚本

---
+++ script.sql
@@ -0,0 +1,13 @@
+create database patient_edge;
+use patient_edge;
+create table temperature(
+	temperature_id       int auto_increment,
+	patient_id varchar(255),  # 传感器的名称
+	value double,      
+	timestamp  datetime,
+	primary key (temperature_id)
+);
+create table patient(
+	patient_id varchar(255),
+	primary key(patient_id)
+);
